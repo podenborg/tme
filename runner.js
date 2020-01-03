502a4223394b08317743123ba31bc3dd7f9c3dcd
Patrick Odenborg <patrick.odenborg@pearson.com>
2020-01-02 21:41:21 -0600

    Add beforeEach and it functions

--- runner.js
+++ runner.js
@@ -1,14 +1,43 @@
 const fs = require('fs');
+const path = require('path');
 
 class Runner {
   constructor() {
-    this.files = [];
+    this.testFiles = [];
+  }
+
+  async runTests() {
+    for (let file of this.testFiles) {
+      const beforeEaches = [];
+
+      global.beforeEach = (fn) => {
+        beforeEaches.push(fn);
+      };
+
+      global.it = (desc, fn) => {
+        beforeEaches.forEach(func => func());
+        fn();
+      };
+
+      require(file.name);
+    }
   }
 
   async collectFiles(targetPath) {
     const files = await fs.promises.readdir(targetPath);
 
-    return files;
+    for (let file of files) {
+      const filepath = path.join(targetPath, file);
+      const stats = await fs.promises.lstat(filepath);
+
+      if (stats.isFile() && file.includes('.test.js')) {
+        this.testFiles.push({ name: filepath });
+      } else if (stats.isDirectory()) {
+        const childFiles = await fs.promises.readdir(filepath);
+
+        files.push(...childFiles.map(f => path.join(file, f)));
+      }
+    }
   }
 }
 
