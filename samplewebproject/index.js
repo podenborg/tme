0a3474b9a57d73578868f8ebd43b059fa8212ac9
Patrick Odenborg <patrick.odenborg@pearson.com>
2020-01-07 13:25:18 -0600

    Final commit

---
+++ samplewebproject/index.js
@@ -0,0 +1,17 @@
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const { value } = document.querySelector('input');
+
+  const header = document.querySelector('h1');
+
+  if (value.includes('@')) {
+    // valid
+    header.innerHTML = 'Looks good!';
+  } else {
+    // invalid
+    header.innerHTML = 'Invalid email';
+  }
+});
+
+window.stuffLoaded = true;
